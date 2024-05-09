9b3111a2a8306026a12d8c565e387a1350e4c4fd
qimengjie <qimengjie@163.com>
2024-05-09 15:47:56 +0800

    fix

--- src/page/index.js
+++ src/page/index.js
@@ -708,7 +708,7 @@ class Page {
         }
 
         if (tagList.length > 1) {
-            templateContent = `<template>\r\n<view style="height:100%;overflow: auto;">\r\n${ templateContent }\r\n</view>\r\n</template>`
+            templateContent = `<template>\r\n<view>\r\n${ templateContent }\r\n</view>\r\n</template>`
         } else if (tagList.length === 1) {
             var firstNode = tagList[0].content
             var attributes = firstNode.attributes
@@ -716,7 +716,7 @@ class Page {
 
             var hasFor = attributes.some(obj => obj.key.content.indexOf("for") > -1)
             if (hasFor || firstNode.name !== "view") {
-                templateContent = `<template>\r\n<view style="height:100%;overflow: auto;">\r\n${ templateContent }\r\n</view>\r\n</template>`
+                templateContent = `<template>\r\n<view>\r\n${ templateContent }\r\n</view>\r\n</template>`
             } else {
                 templateContent = `<template>\r\n${ templateContent }\r\n</template>`
             }
